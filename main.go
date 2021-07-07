bae8ee8a24cbcafdc1a92b691d8200477494439c
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-07-07 18:29:10 -0400

    programa terminado y funcionando

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"hash/fnv"
 	"log"
+	"os"
 	"sort"
 	"strings"
 	"time"
-	"log"
 )
 
 func sortingValues(word string) string {
@@ -30,7 +32,7 @@ func hashValues(values string) int32 {
 
 }
 
-func timerOfProgram(startTimer time.Time, name string)  {
+func timerOfProgram(startTimer time.Time, name string) {
 
 	timeDuration := time.Since(startTimer)
 	log.Printf("%s the program took a time of: %s", name, timeDuration)
@@ -38,6 +40,73 @@ func timerOfProgram(startTimer time.Time, name string)  {
 
 func main() {
 
-	
+	defer timerOfProgram(time.Now(), "anagrams")
+
+	file, err := os.Open("wordList.txt")
+
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+
+	defer file.Close()
+
+	wordsMaps := make(map[int]map[int32][]string)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+
+		words := scanner.Text()
+		wordsLenght := len(words)
+		hashedWords := hashValues(words)
+		wordsList := wordsMaps[wordsLenght][hashedWords]
+		wordsList = append(wordsList, words)
+
+		if wordsMaps[wordsLenght] == nil {
+
+			temporalHash := make(map[int32][]string)
+			temporalHash[hashedWords] = []string{words}
+			wordsMaps[wordsLenght] = temporalHash
+
+		} else {
+
+			wordsMaps[wordsLenght][hashedWords] = wordsList
+
+		}
+	}
+
+	file, err = os.Open("wordList.txt")
+
+	if err != nil {
+
+		log.Fatal(err)
+
+	}
+
+	defer file.Close()
+
+	scanner = bufio.NewScanner(file)
+
+	setsOfAnagramsQuantity := 0
+
+	for scanner.Scan() {
+
+		words := scanner.Text()
+		wordsLenght := len(words)
+		hashedWords := hashValues(words)
+
+		if len(wordsMaps[wordsLenght][hashedWords]) > 1 {
+
+			fmt.Println(wordsMaps[wordsLenght][hashedWords])
+			wordsMaps[wordsLenght][hashedWords] = nil
+
+			setsOfAnagramsQuantity++
+
+		}
+
+	}
+
+	fmt.Println("The quantity of the sets is: ", setsOfAnagramsQuantity)
 
 }
