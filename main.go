ca060a63e3adceb5abbcf108639ce2451eb4a364
Porfirio Rodriguez <porfiriorodt@gmail.com>
2021-07-07 18:14:32 -0400

    agregando test y metodo sort

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	
+	"sort"
+	"strings"
 )
 
+func sortingValues(word string) string {
+
+	words := strings.Split(word, "")
+	sort.Strings(words)
+	return strings.Join(words, "")
+
+}
+
 func main() {
-
-
 
 }
--- main_test.go
+++ main_test.go
@@ -1,2 +1,15 @@
 package main
 
+import "testing"
+
+func TestValuesShouldBeSorted (t *testing.T){
+
+	w := "bac"
+
+	if sortingValues(w) != "abc" {
+
+		t.Error("Values are not getting sorted")
+
+	}
+
+}
